17d0d9c02c047af758b806255ec1e867b965fb51
xXxennoxXx <jakub.kisiala96@gmail.com>
2021-03-28 14:06:35 +0200

    Working build

--- src/main/java/application/Run.java
+++ src/main/java/application/Run.java
@@ -10,9 +10,9 @@ public class Run extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			Parent root = FXMLLoader.load(getClass().getResource("/application/Main.fxml"));
+			Parent root = FXMLLoader.load(getClass().getResource("/Main.fxml"));
 			Scene scene = new Scene(root);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			scene.getStylesheets().add(getClass().getResource("/application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch (Exception e) {
